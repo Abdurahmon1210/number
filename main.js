c7a2530a4f13379b00c4170aa0c053288023f7ff
Abdurhamon <asd@gmail.com>
2022-10-05 11:14:02 +0500

    number

---
+++ main.js
@@ -0,0 +1,13 @@
+var ans = +prompt('son kiriting')
+ans = Number(ans)
+if(isNaN(ans)){
+    alert('bu son emas, iltimos son kiriting!!!')
+}else if(ans == ''){
+    alert('bu son emas, iltimos son kiriting!!!')
+}else if(ans % 2 == 0){
+alert('bu juft son')
+}else{
+    alert('bu toq son')
+}      
+
+
